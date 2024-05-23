c7ce7fddf1931aefc9cb221829fda9e8660d66f6
BrunoLPSilva <brunoluizpimentadasilva1995@gmail.com>
2024-05-22 22:26:14 -0300

    Second migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name  { get; set; }
+        public string Email { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        //NÃO CRIAR CONTRUTOR EM ICOLECTION
+        // Construtor sem parâmetros necessário para o Entity Framework
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        //ADICIONAR VENDA NA LISTA DE VENDA
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            //usando lista de vendas
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
